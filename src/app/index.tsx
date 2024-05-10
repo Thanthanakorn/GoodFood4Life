e0b566f3437cca2a15771490a14b176737e8171c
Thanthanakorn <98535350+Thanthanakorn@users.noreply.github.com>
2024-05-11 02:54:15 +0700

    Food List Item

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,20 +1,22 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-import {Link} from "expo-router";
-
+import {StyleSheet, View} from 'react-native';
+import FoodListItem from "../components/FoodListItem";
 export default function Index() {
-  return (
-    <View style={styles.container}>
-      <Text>This is my project</Text>
-    </View>
-  );
+    return (
+        <View style={styles.container}>
+            {/* Food Item View*/}
+            <FoodListItem item = {{label: "Pizza", cal: 600, brand: "Domino's"}}/>
+            <FoodListItem item = {{label: "Rib eye Stake", cal: 400, brand: "Santa fe"}} />
+
+        </View>
+    );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        justifyContent: 'center',
+        padding: 10,
+        gap: 5,
+    },
 });
